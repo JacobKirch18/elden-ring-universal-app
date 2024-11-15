748cc49aee7a69eda4fdca49e297b00debe54cec
bnejons <bjones33@harding.edu>
2024-11-14 20:51:45 -0700

    Added Stack Panel and details to Main

--- eldenRingUniversalApp/MainPage.xaml.cs
+++ eldenRingUniversalApp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -22,9 +23,29 @@ namespace eldenRingUniversalApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // All of these are tests and should be removed 
+        private ObservableCollection<Boss> bossList;
+        string[] drops = { "30,000 runes" };
+        string margitImagePath = @"Images\margit.jfif"; 
         public MainPage()
         {
             this.InitializeComponent();
+
+            bossList = new ObservableCollection<Boss>()
+            {
+                new Boss { Id = "0", Description = "Test0", Drops = drops,
+                    HealthPoints = "10", Location = "Limgrave", Name = "Margit1",
+                    Image = margitImagePath
+                },
+                new Boss { Id = "1", Description = "Test1", Drops = drops,
+                    HealthPoints = "10", Location = "Limgrave", Name = "Margit2",
+                    Image = margitImagePath
+                },
+                new Boss { Id = "3", Description = "Test3", Drops = drops,
+                    HealthPoints = "10", Location = "Limgrave", Name = "Margit3",
+                    Image = margitImagePath
+                }
+            };
         }
 
         private void compendiumButton_Click(object sender, RoutedEventArgs e)
@@ -35,6 +56,11 @@ namespace eldenRingUniversalApp
         private void aboutGameButton_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(AboutPage));
+        }
+
+        private void defeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            // When user defeats a boss
         }
     }
 }
