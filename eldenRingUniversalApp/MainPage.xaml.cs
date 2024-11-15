d980455bc00d2f6f2f312892005e59f960514c04
bnejons <bjones33@harding.edu>
2024-11-15 13:59:41 -0700

    Added defeatedBosses List, added pop notification

--- eldenRingUniversalApp/MainPage.xaml.cs
+++ eldenRingUniversalApp/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -23,6 +24,9 @@ namespace eldenRingUniversalApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private List<Boss> defeatedBosses = new List<Boss>();
+
         // All of these are tests and should be removed 
         private ObservableCollection<Boss> bossList;
         string[] drops = { "30,000 runes" };
@@ -68,8 +72,35 @@ namespace eldenRingUniversalApp
                 Boss defeatedBoss = clickedButton.DataContext as Boss;
                 if (defeatedBoss != null)
                 {
-                    // Send defeatedBoss to Compendium
-                    // Alert the user maybe?
+                    defeatedBosses.Add(defeatedBoss);
+
+                    // I asked ChatGPT "how could I flash a little notification pop up in my page?" to learn about Popup
+                    // I also asked ChatGPT "I'm trying to have it centered, how could I do that" and got the following code
+
+                    // Get the size of the current window
+                    var windowBounds = Window.Current.Bounds;
+
+                    // Get the Popup's content size
+                    NotificationPopup.Child.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                    var popupSize = NotificationPopup.Child.DesiredSize;
+
+                    // Calculate the center position
+                    double horizontalOffset = (windowBounds.Width - popupSize.Width) / 2;
+                    double verticalOffset = (windowBounds.Height - popupSize.Height) / 2;
+
+                    // Set the Popup's offset
+                    NotificationPopup.HorizontalOffset = horizontalOffset;
+                    NotificationPopup.VerticalOffset = verticalOffset;
+                    NotificationPopup.IsOpen = true;
+
+                    // Automatically close the popup after 3 seconds
+                    Task task = Task.Delay(3000).ContinueWith(t =>
+                    {
+                        _ = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                        {
+                            NotificationPopup.IsOpen = false;
+                        });
+                    });
                     // Remove boss from Main or show that he was defeated in Main
                 }
             }
