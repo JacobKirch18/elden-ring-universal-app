9473f92dba845d844050286d7172848bf1478b9d
Jacob Kirchner <jacobkir500@gmail.com>
2024-11-15 16:17:09 -0600

    Completed about page

--- eldenRingUniversalApp/AboutPage.xaml.cs
+++ eldenRingUniversalApp/AboutPage.xaml.cs
@@ -26,5 +26,10 @@ namespace eldenRingUniversalApp
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.GoBack();
+        }
     }
 }
